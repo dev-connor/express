cca065e8d054d807c4d94c684a5aac971fc6ef2a
Connor <kor.connor@gmail.com>
2023-02-12 20:59:06 +0900

    미들웨어 async 함수로 작성하기

--- src/main.js
+++ src/main.js
@@ -3,22 +3,35 @@
 /* eslint-disable no-console */
 
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 
 const PORT = 5000
 
-app.use('/', (req, res, next) => {
+app.use('/', async (req, res, next) => {
   console.log('Middleware 1')
 
-  setTimeout(() => {
-    next()
-  }, 1000)
+  const fileContent = await fs.promises.readFile('.gitignore')
+
+  const requestedAt = new Date()
+
+  // @ts-ignore
+  req.requestedAt = requestedAt
+  // @ts-ignore
+  req.fileContent = fileContent
+  next()
 })
+
+/* 수 많은 middleware 들... */
 
 app.use((req, res) => {
   console.log('Middleware 2')
-  res.send('Hello, express!')
+
+  res.send(
+    // @ts-ignore
+    `Hello, express!: Requested at ${req.requestedAt}, ${req.fileContent}`
+  )
 })
 
 app.listen(PORT, () => {
